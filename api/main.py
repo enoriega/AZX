026d41f93e473c872847334f0468c49e50f0cb66
asm-tx <142355882+asm-tx@users.noreply.github.com>
2023-09-29 14:31:11 -0700

    Update geocode to handle multiple input paramters

--- api/main.py
+++ api/main.py
@@ -11,16 +11,20 @@ def get_weather_alerts(lat:float,lon:float):
     location = get_alerts(lat,lon)
     return location
 
-@app.get("/geocode")
-def geocode_integrated(address:str):
-    coordinates = geocode(address)
-    print(coordinates)
-    if coordinates == None:
-        return None
-    else:
-        lon = coordinates['x']
-        lat = coordinates['y']
-        return lat, lon
+@app.post("/geocode")
+def geocode_integrated(addresses:list[str]):
+    ret = list()
+    for address in addresses:
+        coordinates = geocode(address) # need to modify to match geocode function
+        if coordinates == None:
+            return None
+        else:
+            lon = coordinates['x']
+            lat = coordinates['y']
+            ret.append({"latitude":lat,"longitude":lon})
+    return ret
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# return nested list of lat, long dictionaries
