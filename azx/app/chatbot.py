16f8cac86dd09d4c4c1861fa34f1016f44a76d96
Enrique Noriega <enoriega@arizona.edu>
2023-10-24 16:17:56 -0700

    Example of the react template

--- azx/app/chatbot.py
+++ azx/app/chatbot.py
@@ -9,10 +9,12 @@ from datetime import datetime
 
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 openai.api_key = os.getenv("OPEN_API_KEY")
 
 llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
+
 
 @dataclass
 class WeatherAlert:
@@ -23,51 +25,55 @@ class WeatherAlert:
     starts: datetime
     expires: datetime
 
-def get_alerts(lat:float, lon:float) -> List[WeatherAlert]:
-    
-	endpoint = "https://api.weather.gov/alerts/active"
 
-	params = {
-		"point": f"{lat},{lon}"
-	}
+def get_alerts(lat: float, lon: float) -> List[WeatherAlert]:
+    endpoint = "https://api.weather.gov/alerts/active"
 
-	response = requests.get(url=endpoint, params=params)
+    params = {
+        "point": f"{lat},{lon}"
+    }
 
-	if response.status_code == 200:
-		# We succeeded
+    response = requests.get(url=endpoint, params=params)
 
-		data = response.json()
+    if response.status_code == 200:
+        # We succeeded
 
-		ret = list()
+        data = response.json()
 
-		for feature in data['features']:
+        ret = list()
 
-			props = feature['properties']
-			
-			alert = WeatherAlert(
-				lat=lat,
-				lon=lon,
-				type=props['event'],
-				description=props['description'],
-				starts=props['onset'],
-				expires=props['expires']
-			)
+        for feature in data['features']:
+            props = feature['properties']
 
-			ret.append(alert)
-		
-		return ret
+            alert = WeatherAlert(
+                lat=lat,
+                lon=lon,
+                type=props['event'],
+                description=props['description'],
+                starts=props['onset'],
+                expires=props['expires']
+            )
+
+            ret.append(alert)
+
+        return ret
+
 
 with gr.Blocks() as demo:
     def ask_cold_weather_question():
         return "How can I protect myself in cold weather?"
+
+
     def ask_desert_question():
         return "How can I protect myself in the desert?"
+
 
     def format_alerts(alerts):
         output = ""
         for i, alert in enumerate(alerts):
-            output += "-"*45 + " Alert " + str(i + 1) + " " + "-"*45 + "\n\n" + alert.description + "\n\n"
+            output += "-" * 45 + " Alert " + str(i + 1) + " " + "-" * 45 + "\n\n" + alert.description + "\n\n"
         return output
+
 
     def process_alerts(lat, lon):
         alerts = get_alerts(lat, lon)
@@ -77,7 +83,8 @@ with gr.Blocks() as demo:
         if alerts == []:
             return "No alerts currently in this area!"
         return format_alerts(alerts)
-    
+
+
     def predict(message, history):
         history_langchain_format = []
         for human, ai in history:
@@ -87,10 +94,12 @@ with gr.Blocks() as demo:
         gpt_response = llm(history_langchain_format)
         return gpt_response.content
 
+
     def respond(message, chat_history):
         bot_message = predict(message, chat_history)
         chat_history.append((message, bot_message))
         return "", chat_history
+
 
     with gr.Row():
         with gr.Column():
@@ -99,14 +108,14 @@ with gr.Blocks() as demo:
             clear = gr.ClearButton([msg, chatbot])
         with gr.Column():
             with gr.Row():
-                cold_weather_button = gr.Button(ask_cold_weather_question, value="Ask about Cold Weather")
-                desert_button = gr.Button(ask_desert_question, value="Ask about the Desert")
+                cold_weather_button = gr.Button(ask_cold_weather_question, label="Ask about Cold Weather")
+                desert_button = gr.Button(ask_desert_question, label="Ask about the Desert")
             lat = gr.Textbox(label="Latitude", type="text")
             lon = gr.Textbox(label="Longitude", type="text")
             gr.Interface(
-                fn = process_alerts,
-                inputs = [lat, lon],
-                outputs = [gr.Textbox(label="Alerts", type="text")]
+                fn=process_alerts,
+                inputs=[lat, lon],
+                outputs=[gr.Textbox(label="Alerts", type="text")]
             )
 
             msg.submit(respond, [msg, chatbot], [msg, chatbot])
