9485ae95c8463fab56ae8c6315754ae2910f2e81
Daniel I. Leon <danielileon@Daniels-MacBook-Pro.local>
2023-11-12 13:44:16 -0700

    Initial Gradio chatbot

--- azx/app/chatbot.py
+++ azx/app/chatbot.py
@@ -57,61 +57,91 @@ def get_alerts(lat:float, lon:float) -> List[WeatherAlert]:
 		
 		return ret
 
+def format_alerts(alerts):
+    outputs = []
+    for i, alert in enumerate(alerts):
+        output = str(alert.type)
+        outputs.append(output)
+    #return "\n\n".join(outputs)
+    return outputs
+
+def process_alerts(lat, lon):
+    alerts = get_alerts(lat, lon)
+
+    if alerts is None:
+        return "Search unsuccessful. Please try again."
+    if alerts == []:
+        return "No alerts currently in this area!"
+    return format_alerts(alerts)
+
+def alert_chatbot(dropdown, chat_history):
+    message = f"The NWS has issued an alert of type {dropdown} in my area. What should I do?" 
+    bot_message = predict(message, chat_history)
+    chat_history.append((message, bot_message))
+    return "", chat_history
+
+def predict(message, history):
+    history_langchain_format = []
+    for human, ai in history:
+        history_langchain_format.append(HumanMessage(content=human))
+        history_langchain_format.append(AIMessage(content=ai))
+    history_langchain_format.append(HumanMessage(content=message))
+    gpt_response = llm(history_langchain_format)
+    return gpt_response.content
+
+def respond(message, chat_history):
+    bot_message = predict(message, chat_history)
+    chat_history.append((message, bot_message))
+    return "", chat_history
+
+def update_dropdown(lat, lon):
+    # For testing/demonstration purposes
+    if lat == "10" and lon == "10":
+        alerts = ["Heat Advisory", "Drought Advisory"]
+        return gr.Dropdown(alerts, 
+            label="Select an Alert in your Area", 
+            allow_custom_value=True, 
+            value=alerts[0]
+        )
+
+    alerts = process_alerts(lat, lon)
+
+    if isinstance(alerts, list):
+        return gr.Dropdown(alerts, 
+            label="Select an Alert in your Area", 
+            allow_custom_value=True, 
+            value=alerts[0]
+        )
+
+    none_found = ["NO ALERT FOUND"]
+    return gr.Dropdown(none_found, 
+        label="Alert not found. Try different coordinates", 
+        allow_custom_value=True,
+        value=none_found[0]
+    )
+
 with gr.Blocks() as demo:
-    def ask_cold_weather_question():
-        return "How can I protect myself in cold weather?"
-    def ask_desert_question():
-        return "How can I protect myself in the desert?"
-
-    def format_alerts(alerts):
-        output = ""
-        for i, alert in enumerate(alerts):
-            output += "-"*45 + " Alert " + str(i + 1) + " " + "-"*45 + "\n\n" + alert.description + "\n\n"
-        return output
-
-    def process_alerts(lat, lon):
-        alerts = get_alerts(lat, lon)
-
-        if alerts is None:
-            return "Search unsuccessful. Please try again."
-        if alerts == []:
-            return "No alerts currently in this area!"
-        return format_alerts(alerts)
-    
-    def predict(message, history):
-        history_langchain_format = []
-        for human, ai in history:
-            history_langchain_format.append(HumanMessage(content=human))
-            history_langchain_format.append(AIMessage(content=ai))
-        history_langchain_format.append(HumanMessage(content=message))
-        gpt_response = llm(history_langchain_format)
-        return gpt_response.content
-
-    def respond(message, chat_history):
-        bot_message = predict(message, chat_history)
-        chat_history.append((message, bot_message))
-        return "", chat_history
-
     with gr.Row():
+        with gr.Column():
+            lat = gr.Textbox(label="Latitude", type="text")
+            lon = gr.Textbox(label="Longitude", type="text")
+
+            dropdown_button = gr.Button(value="Enter coordinates")
+            dropdowns = [gr.Dropdown([], label="Complete above form", allow_custom_value=True)]
+
+            advice_button = gr.Button(value="Click for Chatbot Advice")
+
         with gr.Column():
             chatbot = gr.Chatbot()
             msg = gr.Textbox()
-            clear = gr.ClearButton([msg, chatbot])
-        with gr.Column():
-            with gr.Row():
-                cold_weather_button = gr.Button(ask_cold_weather_question, value="Ask about Cold Weather")
-                desert_button = gr.Button(ask_desert_question, value="Ask about the Desert")
-            lat = gr.Textbox(label="Latitude", type="text")
-            lon = gr.Textbox(label="Longitude", type="text")
-            gr.Interface(
-                fn = process_alerts,
-                inputs = [lat, lon],
-                outputs = [gr.Textbox(label="Alerts", type="text")]
-            )
 
-            msg.submit(respond, [msg, chatbot], [msg, chatbot])
-            cold_weather_button.click(respond, [cold_weather_button, chatbot], [msg, chatbot])
-            desert_button.click(respond, [desert_button, chatbot], [msg, chatbot])
+        msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+        lat.submit(update_dropdown, [lat, lon], [dropdowns[0]])
+        lon.submit(update_dropdown, [lat, lon], [dropdowns[0]])
+        dropdown_button.click(update_dropdown, [lat, lon], [dropdowns[0]])
+
+        advice_button.click(alert_chatbot, [dropdowns[0], chatbot], [msg, chatbot])
 
 if __name__ == "__main__":
     demo.launch()
