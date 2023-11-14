060bfbe00474f66ffbf1a49631c0cedff21f5f9d
Enrique Noriega <enoriega@arizona.edu>
2023-11-13 23:39:39 -0700

    Added RAG chain and initial data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+Added ---
+title: AZX
+app_file: azx/app/chatbot.py
+sdk: gradio
+sdk_version: 3.43.2
+---
 # AZX
 AZX System from AIRES grant
--- azx/app/chatbot.py
+++ azx/app/chatbot.py
@@ -7,12 +7,15 @@ import requests
 from dataclasses import dataclass
 from datetime import datetime
 
+from rag import build_rag_chain
 import os
-from dotenv import load_dotenv
-load_dotenv()
-openai.api_key = os.getenv("OPEN_API_KEY")
 
-llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+llm = ChatOpenAI(temperature=0, model='gpt-4-1106-preview')
+
+# Import the rag chain
+rag_chain = build_rag_chain("azx_data.tsv", llm)
 
 @dataclass
 class WeatherAlert:
@@ -23,47 +26,51 @@ class WeatherAlert:
     starts: datetime
     expires: datetime
 
-def get_alerts(lat:float, lon:float) -> List[WeatherAlert]:
-    
-	endpoint = "https://api.weather.gov/alerts/active"
 
-	params = {
-		"point": f"{lat},{lon}"
-	}
 
-	response = requests.get(url=endpoint, params=params)
 
-	if response.status_code == 200:
-		# We succeeded
 
-		data = response.json()
+def get_alerts(lat: float, lon: float) -> List[WeatherAlert]:
+    endpoint = "https://api.weather.gov/alerts/active"
 
-		ret = list()
+    params = {
+        "point": f"{lat},{lon}"
+    }
 
-		for feature in data['features']:
+    response = requests.get(url=endpoint, params=params)
 
-			props = feature['properties']
-			
-			alert = WeatherAlert(
-				lat=lat,
-				lon=lon,
-				type=props['event'],
-				description=props['description'],
-				starts=props['onset'],
-				expires=props['expires']
-			)
+    if response.status_code == 200:
+        # We succeeded
 
-			ret.append(alert)
-		
-		return ret
+        data = response.json()
+
+        ret = list()
+
+        for feature in data['features']:
+            props = feature['properties']
+
+            alert = WeatherAlert(
+                lat=lat,
+                lon=lon,
+                type=props['event'],
+                description=props['description'],
+                starts=props['onset'],
+                expires=props['expires']
+            )
+
+            ret.append(alert)
+
+        return ret
+
 
 def format_alerts(alerts):
     outputs = []
     for i, alert in enumerate(alerts):
         output = str(alert.type)
         outputs.append(output)
-    #return "\n\n".join(outputs)
+    # return "\n\n".join(outputs)
     return outputs
+
 
 def process_alerts(lat, lon):
     alerts = get_alerts(lat, lon)
@@ -74,11 +81,13 @@ def process_alerts(lat, lon):
         return "No alerts currently in this area!"
     return format_alerts(alerts)
 
+
 def alert_chatbot(dropdown, chat_history):
-    message = f"The NWS has issued an alert of type {dropdown} in my area. What should I do?" 
+    message = f"The NWS has issued an alert of type {dropdown} in my area. What should I do?"
     bot_message = predict(message, chat_history)
     chat_history.append((message, bot_message))
     return "", chat_history
+
 
 def predict(message, history):
     history_langchain_format = []
@@ -86,39 +95,47 @@ def predict(message, history):
         history_langchain_format.append(HumanMessage(content=human))
         history_langchain_format.append(AIMessage(content=ai))
     history_langchain_format.append(HumanMessage(content=message))
-    gpt_response = llm(history_langchain_format)
-    return gpt_response.content
+    # If the chat is empty (i.e. new interaction), use the rag chain, otherwise use the llm directly
+    if len(history) == 0:
+        first_response = rag_chain.invoke(message)
+        return first_response.content
+    else:
+        gpt_response = llm(history_langchain_format)
+        return gpt_response.content
+
 
 def respond(message, chat_history):
     bot_message = predict(message, chat_history)
     chat_history.append((message, bot_message))
     return "", chat_history
 
+
 def update_dropdown(lat, lon):
     # For testing/demonstration purposes
     if lat == "10" and lon == "10":
-        alerts = ["Heat Advisory", "Drought Advisory"]
-        return gr.Dropdown(alerts, 
-            label="Select an Alert in your Area", 
-            allow_custom_value=True, 
-            value=alerts[0]
-        )
+        alerts = ["Heat Advisory", "Drought Advisory", "Hard Freeze Warning"]
+        return gr.Dropdown(alerts,
+                           label="Select an Alert in your Area",
+                           allow_custom_value=True,
+                           value=alerts[0]
+                           )
 
     alerts = process_alerts(lat, lon)
 
     if isinstance(alerts, list):
-        return gr.Dropdown(alerts, 
-            label="Select an Alert in your Area", 
-            allow_custom_value=True, 
-            value=alerts[0]
-        )
+        return gr.Dropdown(alerts,
+                           label="Select an Alert in your Area",
+                           allow_custom_value=True,
+                           value=alerts[0]
+                           )
 
     none_found = ["NO ALERT FOUND"]
-    return gr.Dropdown(none_found, 
-        label="Alert not found. Try different coordinates", 
-        allow_custom_value=True,
-        value=none_found[0]
-    )
+    return gr.Dropdown(none_found,
+                       label="Alert not found. Try different coordinates",
+                       allow_custom_value=True,
+                       value=none_found[0]
+                       )
+
 
 with gr.Blocks() as demo:
     with gr.Row():
