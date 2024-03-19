e05e89ef1f71fb886483b0ddb2949e41144ea9cb
Enrique Noriega <enoriega@arizona.edu>
2024-03-19 15:49:19 -0700

    Implemented inclusion of URL attributions from the vector database

--- azx/app/rag.py
+++ azx/app/rag.py
@@ -1,22 +1,15 @@
 import csv
-import itertools as it
-import os
 
-import openai
 from langchain.prompts import PromptTemplate
 from langchain.schema import Document
+from langchain.schema.runnable import RunnablePassthrough, RunnableParallel
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
-from langchain import hub
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.chat_models import ChatOpenAI
+from langchain_community.vectorstores import Chroma
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_core.output_parsers import StrOutputParser
 
-
-from dotenv import load_dotenv
-
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def load_data(path: str):
     with open(path) as f:
@@ -25,26 +18,63 @@ def load_data(path: str):
 
     return ret
 
-def build_rag_chain(path:str, llm):
-    documents = load_data(path)
+def parse_docs(documents):
+    ret = list()
+    for d in documents:
+        data = f"Document: ```{d.page_content}```"
+        if 'URL' in d.metadata:
+            data += f"\nURL: {d.metadata['URL']}"
+        ret.append(data)
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    splits = text_splitter.split_documents(documents)
-    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    return ret
+
+
+def build_rag_chain(path: str, llm):
+    # documents = load_data(path)
+    #
+    # text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    # splits = text_splitter.split_documents(documents)
+    # vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vectorstore = Chroma(persist_directory=path, embedding_function=embeddings)
     # rag_prompt = hub.pull("rlm/rag-prompt")
-    rag_prompt = PromptTemplate.from_template("You are a public health advocate. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Enumerate the actionable items as clear and consise bullets in a list. Write a follow up sentence that briefly elaborates the main sentence in each item.\nQuestion: {question} \nContext: {context} \nAnswer:")
+    rag_prompt = PromptTemplate.from_template(
+        "You are a public health advocate. Use the following documents to answer the question. If "
+        "you don't know the answer, just say that you don't know. Enumerate the actionable items as clear and concise. For each action item, if there is a relevant document that includes a URL, include the URL for reference. "
+        "bullets in a list. Write a follow up sentence that briefly elaborates the main sentence in each."
+        "item.\nQuestion: {question} \nContext: {context} \nAnswer:")
     retriever = vectorstore.as_retriever()
-    # llm = ChatOpenAI(temperature=0, model='gpt-4-1106-preview')
-    rag_chain = {"context": retriever, "question": RunnablePassthrough()} | rag_prompt | llm
+
+    rag_chain = ({
+                     "context": retriever | RunnableLambda(parse_docs),
+                     "question": RunnablePassthrough()
+                 }
+
+                 | rag_prompt | llm)
 
     return rag_chain
 
 
 if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model='gpt-4-1106-preview')
-    rag_chain = build_rag_chain("azx_data.tsv", llm)
+    # rag_chain = build_rag_chain("azx_data.tsv", llm)
+    chain = build_rag_chain("/Users/enoriega/github/AZX/scripts/resources", llm)
 
-    x = rag_chain.invoke("What should I do in the presence of poor air quality?")
+    # x = rag_chain.invoke("What should I do in the presence of poor air quality?")
 
-    print(x)
+    from fastapi import FastAPI
+    from langserve import add_routes
+    import uvicorn
 
+    app = FastAPI(
+        title="LangChain Server",
+        version="1.0",
+        description="A simple api server using Langchain's Runnable interfaces",
+    )
+
+    add_routes(
+        app,
+        chain
+    )
+
+    uvicorn.run(app, host="localhost", port=8000)
